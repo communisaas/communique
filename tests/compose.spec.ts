dc3c32db9210901d38419ff3b6520d13025389ae
ejmockler <emockler@ucsc.edu>
2023-02-22 18:23:06 -0800

    determine validation cases

--- tests/compose.spec.ts
+++ tests/compose.spec.ts
@@ -1,3 +1,4 @@
+import { browser } from '$app/environment';
 import { expect, test as base, chromium, type Page, type Locator } from '@playwright/test';
 
 let page: Page;
@@ -32,7 +33,6 @@ base.describe('tag', () => {
 			tagInterface
 		}) => {
 			// invisible by default
-
 			await expect(input).toHaveCSS('width', '0px');
 			// toggle visibility
 			for (const visible of [true, false]) {
@@ -45,4 +45,64 @@ base.describe('tag', () => {
 			}
 		});
 	}
+});
+
+base.describe('post', () => {
+	type EmailFixture = {
+		topic: string[];
+		recipient: string[];
+		subject: string;
+		body: string;
+		postStatus: Response;
+	};
+	const test = base.extend<EmailFixture>({
+		topic: async ({}, use) => {
+			await use(['topic 1', 'topic 2']);
+		},
+		recipient: async ({}, use) => {
+			await use(['email@first.com', 'email@next.com']);
+		},
+		subject: async ({}, use) => {
+			await use("let's talk about: this");
+		},
+		body: async ({}, use) => {
+			// TODO body placeholders
+			await use(`
+				<h1>Hello,</h1>
+				<p>thank you for reading!</p>
+				<span>Best regards,<i>{name}</i></span>
+
+			`);
+		},
+		postStatus: async ({ page }, use) => {
+			await page.route('**/compose/publish', (route) =>
+				route.fulfill({
+					status: 200,
+					body: 'accept'
+				})
+			);
+		}
+	});
+
+	for (const placeholderText of ['Recipient', 'Topic']) {
+		test(`Missing ${placeholderText.toLowerCase()}s caught`, async ({
+			topic,
+			recipient,
+			body,
+			postStatus,
+			page
+		}) => {});
+	}
+
+	test(`Missing subject caught`, async ({ topic, recipient, body, postStatus, page }) => {});
+
+	test(`Missing body caught`, async ({ topic, recipient, body, postStatus, page }) => {});
+
+	test(`Post success confirmation appears after response`, async ({
+		topic,
+		recipient,
+		body,
+		postStatus,
+		page
+	}) => {});
 });
