a5986ab770f692be1c35eb4db6e6302a3de9ac46
ejmockler <emockler@ucsc.edu>
2023-03-10 22:37:31 -0800

    add panel component
    this is the root component for browsing emails

---
+++ src/components/Panel.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import Selector from './Selector.svelte';
+	import type { ComponentType } from 'svelte';
+
+	export let header: string;
+	export let selectable: ComponentType;
+	export let selected: Selectable;
+	export let items: string[];
+</script>
+
+<h1>{header}</h1>
+<Selector {selectable} {items} bind:selected />
+
+<style>
+	h1 {
+		text-align: right;
+		margin: -0.25em 1em 0.5em 1em;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script type="ts">
 	import Email from '$components/Email.svelte';
-	import Selector from '$components/Selector.svelte';
+	import Panel from '$components/Panel.svelte';
 	import { onMount } from 'svelte';
 	import type { Writable } from 'svelte/store';
 
@@ -20,11 +20,16 @@
 	<meta name="description" content="Write & share email templates!" />
 </svelte:head>
 
-<h1>Loudest voices in {$store ? $store.topic.name : '{PLACEHOLDER}'}</h1>
-
-{#if store}
-	<Selector selectable={Email} items={data.emailList} bind:selected={$store.email} />
-{/if}
+<section class="flex flex-col">
+	{#if store}
+		<Panel
+			header={`Loudest voices in ${$store ? $store.topic.name : '{PLACEHOLDER}'}`}
+			selectable={Email}
+			items={data.emailList}
+			bind:selected={$store.email}
+		/>
+	{/if}
+</section>
 
 <style>
 	h1 {
