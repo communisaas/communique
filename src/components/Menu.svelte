24087831e9c838c1865a4e8a802b9f5fa0573335
ejmockler <emockler@ucsc.edu>
2023-06-13 17:05:55 -0700

    fix menu item sizing to stretch

--- src/components/Menu.svelte
+++ src/components/Menu.svelte
@@ -4,16 +4,12 @@
 </script>
 
 <aside
-	class="menu rounded absolute top-0 right-0 min-h-full min-w-full z-20"
+	class="menu rounded absolute top-0 left-0 min-h-full min-w-full z-20 flex justify-center"
 	in:fade={{ delay: 25, duration: 300, easing: expoIn }}
 	out:fade={{ delay: 50, duration: 300, easing: expoOut }}
 >
-	<div
-		transition:slide={{ delay: 50, axis: 'x', easing: quintOut }}
-		on:mousedown|stopPropagation
-		class="pt-[0.75rem] max-w-full"
-	>
-		<ul class="flex flex-col gap-y-2">
+	<div on:mousedown|stopPropagation class="pt-[0.75rem] w-full">
+		<ul transition:slide={{ delay: 50, axis: 'x', easing: quintOut }} class="flex flex-col gap-y-2">
 			<slot />
 		</ul>
 	</div>
