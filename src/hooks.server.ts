b158889264cb5f841e25b48c2e0716eae725e135
ejmockler <emockler@ucsc.edu>
2023-10-07 16:30:29 -0700

    switch endpoint cases

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -5,13 +5,21 @@ import { sequence } from '@sveltejs/kit/hooks';
 
 async function authorize({ event, resolve }) {
 	// TODO structure auth requirements, verify session
-	if (event.url.pathname.startsWith('/compose') || event.request.headers.get('increment-send')) {
-		const session = await event.locals.getSession();
-		if (!session) {
-			throw redirect(302, '/sign/in?callbackUrl=/compose');
-		}
-		// TODO update user account in db
-		console.log(session);
+	const session = await event.locals.getSession();
+
+	switch (event) {
+		case event.url.pathname.startsWith('/compose'):
+			if (!session) {
+				throw redirect(302, '/sign/in?callbackUrl=/compose');
+			}
+			// TODO update user account in db
+			console.log(session);
+			break;
+		case event.request.headers.get('increment-send'):
+			if (!session) {
+				throw redirect(302, '/sign/in?callbackUrl=/');
+			}
+			break;
 	}
 
 	return resolve(event);
